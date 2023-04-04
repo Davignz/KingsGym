04f6760a3e7ed8c46f33827adb2e14866a48df05
Davi <werdavid1@gmail.com>
2023-04-04 17:41:10 -0300

    Planos finalizado, corrigir Contato

--- KingsGym/src/js/home.js
+++ KingsGym/src/js/home.js
@@ -9,56 +9,31 @@ btnMobile.addEventListener('click', function() {
 function calcularIMC() {
   var altura = document.getElementById("height").value / 100;
   var peso = document.getElementById("weight").value;
-  var imc = peso / (altura * altura);
   
   if (altura && peso) {
+    var imc = peso / (altura * altura);
     document.getElementById("imc").innerHTML = imc.toFixed(2);
 
     if (imc < 18.5) {
       document.getElementById("imc").classList.add("abaixo-peso");
-      document.getElementById("imc-message").innerHTML = "Abaixo do peso";
+      document.getElementById("resultado").innerHTML = "Abaixo do peso";
     } else if (imc < 25) {
       document.getElementById("imc").classList.add("peso-normal");
-      document.getElementById("imc-message").innerHTML = "Peso normal";
+      document.getElementById("resultado").innerHTML = "com Peso normal";
     } else if (imc < 30) {
       document.getElementById("imc").classList.add("acima-peso");
-      document.getElementById("imc-message").innerHTML = "Acima do peso";
+      document.getElementById("resultado").innerHTML = "Acima do peso";
     } else {
       document.getElementById("imc").classList.add("obesidade");
-      document.getElementById("imc-message").innerHTML = "Obesidade";
+      document.getElementById("resultado").innerHTML = "Obeso";
     }
   } else {
     alert("Por favor, preencha os campos de altura e peso.");
   }
 }
-/*
+
 var form = document.querySelector("form");
 form.addEventListener("submit", function(event) {
   event.preventDefault();
   calcularIMC();
 });
-
-
-const slider = document.querySelector('.slider');
-const prevBtn = document.querySelector('.prev-btn');
-const nextBtn = document.querySelector('.next-btn');
-
-let scrollPosition = 0;
-const planWidth = document.querySelector('.plan').clientWidth;
-const numPlans = slider.children.length;
-const maxScrollPosition = (numPlans - 1) * (planWidth + 20);
-
-prevBtn.addEventListener('click', () => {
-  if (scrollPosition > 0) {
-    scrollPosition -= planWidth + 20;
-    slider.style.transform = `translateX(-${scrollPosition}px)`;
-  }
-});
-
-nextBtn.addEventListener('click', () => {
-  if (scrollPosition < maxScrollPosition) {
-    scrollPosition += planWidth + 20;
-    slider.style.transform = `translateX(-${scrollPosition}px)`;
-  }
-});
-*/
