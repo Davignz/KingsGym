9be045299b73a363bbe447c557142f5e0dc6187a
Davi <werdavid1@gmail.com>
2023-04-04 22:34:59 -0300

    Desk e mobile pronto

--- KingsGym/src/js/home.js
+++ KingsGym/src/js/home.js
@@ -1,18 +1,49 @@
 const btnMobile = document.querySelector('.btn-mobile');
 const nav = document.querySelector('#nav');
 const links = document.querySelectorAll('.menu a');
+const btnToggle = document.querySelector('.dark-mode-toggle');
+const header = document.querySelector('.header');
+const htitle = document.querySelector('.htitle');
+const bar = document.querySelectorAll('.bar');
+const plan = document.querySelectorAll('.plan');
+const imcCalculator = document.querySelector('.imc-calculator');
+const loginSection = document.querySelector('.login-section');
+const menu = document.querySelector('.menu');
 
+// abrir e fechar menu
 btnMobile.addEventListener('click', function() {
   nav.classList.toggle('active');
   btnMobile.classList.toggle('active');
 });
 
+// quando clica no link, já redireciona e fecha o menu
 links.forEach(function(link) {
   link.addEventListener('click', function() {
     nav.classList.remove('active');
     btnMobile.classList.remove('active');
   });
 });
+
+btnToggle.onclick = function(){
+  this.classList.toggle('dark');
+  header.classList.toggle('dark');
+  bar.forEach(element => {
+    element.classList.toggle('dark');
+  });
+  plan.forEach(element => {
+    element.classList.toggle('dark');
+  });
+  links.forEach(element => {
+    element.classList.toggle('dark');
+  });
+  htitle.classList.toggle('dark');
+  btnMobile.classList.toggle('dark');
+  imcCalculator.classList.toggle('dark');
+  loginSection.classList.toggle('dark');
+  nav.classList.toggle('dark');
+  menu.classList.toggle('dark');
+}
+
 
 
 function calcularIMC() {
